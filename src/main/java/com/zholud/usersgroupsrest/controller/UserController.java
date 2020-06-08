38905c4519b23ff68c08b3b7a4b967f0ffb84542
Fedor Zholud <fedor.zholud@isax.com>
2020-06-08 17:29:04 +0200

    add interface for controller

--- src/main/java/com/zholud/usersgroupsrest/controller/UserController.java
+++ src/main/java/com/zholud/usersgroupsrest/controller/UserController.java
@@ -1,27 +1,15 @@
 package com.zholud.usersgroupsrest.controller;
 
 import com.zholud.usersgroupsrest.model.impl.UserEntity;
-import com.zholud.usersgroupsrest.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
-@RestController
-public class UserController {
+@RequestMapping("/users")
+public interface UserController {
 
-    @Autowired
-    private UserService userService;
-
-    @GetMapping(value = "/users")
-    public ResponseEntity<List<UserEntity>> read() {
-        final List<UserEntity> userEntities = userService.findAll();
-
-        return userEntities != null && !userEntities.isEmpty()
-                ? new ResponseEntity<>(userEntities, HttpStatus.OK)
-                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
-    }
+    @GetMapping("/")
+    ResponseEntity<List<UserEntity>> read();
 }
---
+++ src/main/java/com/zholud/usersgroupsrest/controller/impl/UserControllerImpl.java
@@ -0,0 +1,29 @@
+package com.zholud.usersgroupsrest.controller.impl;
+
+import com.zholud.usersgroupsrest.controller.UserController;
+import com.zholud.usersgroupsrest.model.impl.UserEntity;
+import com.zholud.usersgroupsrest.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserControllerImpl implements UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public ResponseEntity<List<UserEntity>> read() {
+        final List<UserEntity> userEntities = userService.findAll();
+
+        return userEntities != null && !userEntities.isEmpty()
+                ? new ResponseEntity<>(userEntities, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+}
